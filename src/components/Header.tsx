d2e5ffbb1edb75368e9b29def56306415204bec5
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-03-01 11:07:43 +0500

    Home page created

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1 className='header-h1' >virtual lollipop</h1>
+            <p className='header-para' >because we all know someone who deserves some sugar</p>
+        </div>
+    )
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import React from "react"
+import Header from "../components/Header"
 import Lolly from "../components/Lolly"
+import { navigate } from 'gatsby'
 //@ts-ignore
-
 
 export default function Home() {
   return (
-  <div>Hello world!
-    <Lolly lolyTop="#8C0040" lolyMiddle="#67000D" lolyBottom="#A7563C" />
-    <Lolly lolyTop="red" lolyMiddle="orange" lolyBottom="yellow" />
-  </div>
+    <div className='container'>
+      <Header />
+      <div className="listLollies">
+        <Lolly lolyTop="#8C0040" lolyMiddle="#67000D" lolyBottom="#A7563C" />
+        <Lolly lolyTop="red" lolyMiddle="orange" lolyBottom="yellow" />
+        <Lolly lolyTop="aqua" lolyMiddle="rgb(0, 255, 42)" lolyBottom="rgb(255, 0, 242)" />
+        <Lolly lolyTop="rgb(224, 46, 70)" lolyMiddle="rgb(46, 58, 224)" lolyBottom="rgb(173, 46, 224)" />
+        <Lolly lolyTop="rgb(224, 46, 156)" lolyMiddle="rgb(224, 153, 46)" lolyBottom="rgb(110, 37, 15)" />
+        <Lolly lolyTop="rgb(15, 110, 94)" lolyMiddle="rgb(3, 17, 77)" lolyBottom="rgb(187, 206, 13)" />
+      </div>
+      <button onClick={
+        ()=>{
+          navigate('/createNew')
+        }
+      } className='navigate-btn' type='button' value='create new loli' >Create new lolli</button>
+    </div>
   )
 }
